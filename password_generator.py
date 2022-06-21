77477dc41d21991b01f142560301e2d7dd1f4806
Mert Kızılyar <91737596+mertkzlyr@users.noreply.github.com>
2022-06-22 00:43:36 +0300

    Update password_generator.py
    Recoded using as few libraries as possible.

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,30 @@
-import string
-import secrets
+import random
 import pyperclip
 
-pass_lenght = input("How many characters do you want in your password? (default 20 for strong password)\n")
-pass_lenght = 20 if pass_lenght == '' else int(pass_lenght)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-alphabet = string.digits + string.ascii_letters + string.punctuation
+print("Welcome to the Password Generator!")
+user_letters= int(input("How many letters would you like in your password?\n")) 
+user_symbols = int(input("How many symbols would you like?\n"))
+user_numbers = int(input("How many numbers would you like?\n"))
 
-while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(pass_lenght))
-    if  any(char.islower() for char in password):
-        any(char.isupper() for char in password)
-        sum(char.isdigit() for char in password)
-        break
+password_list = []
+for letter in range(user_letters + 1):
+  password_list.append(random.choice(letters))
+
+for num in range(user_numbers + 1):
+  password_list.append(random.choice(numbers))
+
+for sym in range(user_symbols + 1):
+  password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
 
 pyperclip.copy(password)
 
